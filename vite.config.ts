67aeddbd93c9275f1fa9effbd966aef0589e9e06
Nikolai Maidan <axroit.08@gmail.com>
2023-09-04 13:18:17 +0800

    build: set of vite builder

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import webExtension from "@samrum/vite-plugin-web-extension";
+import { getManifest } from './src/utils/manifest';
 
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    root: 'src',
+    build: {
+      outDir: '../dist',
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    plugins: [
+      vue(),
+      webExtension({
+        manifest: getManifest(Number(env.MANIFEST_VERSION)),
+      }),
+    ]
+  }
+});
