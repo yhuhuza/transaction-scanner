22ec428138e60f143451cfbb071bc3740e688391
Nikolai Maidan <axroit.08@gmail.com>
2023-09-05 11:25:08 +0800

    chore: change some rules in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
       browser: true,
       es2021: true
   },
-  extends: [ "eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended", "plugin:vue/vue3-recommended" ],
+  extends: [ "eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended", "plugin:vue/vue3-recommended", "prettier"],
   plugins: [ "@typescript-eslint", "prettier", "vue", "eslint-plugin-import" ],
   parser: "vue-eslint-parser",
   parserOptions: {
@@ -26,22 +26,16 @@ module.exports = {
       }
     }],
     'import/newline-after-import': 'error',
-    'no-console': 'error',
+    'no-console': 'warn',
     '@typescript-eslint/no-unused-vars': 'error',
     'semi': ['error', 'always'],
     'comma-dangle': ['error', 'only-multiline'],
     'vue/multi-word-component-names': 0,
     'vue/no-deprecated-dollar-scopedslots-api': 0,
     'vue/require-default-prop': 0,
-    'prettier-vue/prettier': ['error', {
-      singleQuote: true,
-      htmlWhitespaceSensitivity: 'ignore',
-      printWidth: 120
-    }],
     'vue/no-multiple-template-root': 'off'
   },
   "overrides": [{
-    // enable the rule specifically for TypeScript files
     "files": ["*.ts"],
     "rules": {
       "@typescript-eslint/explicit-function-return-type": "error"
