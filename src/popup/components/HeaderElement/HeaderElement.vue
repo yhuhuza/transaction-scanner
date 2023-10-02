dc2e3c933de2bacabbb67b60959d2f64534f6be8
Nikolai Maidan <axroit.08@gmail.com>
2023-10-02 22:38:05 +0800

    dev: header element

--- src/popup/components/HeaderElement/HeaderElement.vue
+++ src/popup/components/HeaderElement/HeaderElement.vue
@@ -1,9 +1,19 @@
 <template>
     <header class="header-element">
-      <div class="header-element__logo block"></div>
-      <button class="header-element__input block">Input</button>
-      <div class="header-element__settings block"></div>
-      </header>
+      <div class="header-element__wrapper">
+        <img src="../../../assets/logo/Logotype.svg" />
+      </div>
+      <select id="select" class="header-element__select" name="plan">
+        <option value="none" selected>Tether TRC20</option>
+        <option value="free">Bitcoin</option>
+        <option value="starter">Ethereum</option>
+        <option value="professional">Toncoin</option>
+        <option value="corporate">Shiba Inu</option>
+      </select>
+      <div class="header-element__settings">
+        <img src="../../../assets/logo/settings-sign.svg" />
+      </div>
+    </header>
 </template>
   
 <script lang="ts" setup></script>
@@ -11,47 +21,38 @@
 <style lang="scss" scoped>
 
 .header-element {
-  box-sizing: border-box;
-  padding: 15px;
-  height: 15vh;
-  background-color: gold;
-  border-bottom: 2px solid black;
+  position: relative;
   display: flex;
-  flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  .block {
-    margin: 15px;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-  } 
-  &__logo {
-    width: 140px;
-    border: 2px solid #000000;
-    background-color:#000000 !important;
-    border-radius: 50%;
-    height: 70px;
-    background: url('../../../assets/logo/logo-icon.png') center no-repeat;
-    background-size: 50px;
-  }
-  &__input {
-    width: 360px;
+  padding: 20px;
+  &__select {
+    box-sizing: border-box;
+    position: relative;
+    border: none;
+    padding: 10px;
+    width: 200px;
     height: 40px;
-    font-weight: 900;
-    border: 2px solid black;
-    border-radius: 20px;
+    background-color: #03B7E2;
+    border-radius: 16px;
+    color: #FFFFFF;
     font-size: 16px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    font-weight: 600;
   }
   &__settings {
-    width: 100px;
-    height: 100px;
-    background: url('../../../assets/logo/settings-sign.png') center no-repeat;
-    background-size: contain;
+    cursor: pointer;
+    width: 30px;
+    height: 30px;
   }
 }
 
-  </style>
+ul, ol{
+	list-style: none;
+	padding: 0;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
