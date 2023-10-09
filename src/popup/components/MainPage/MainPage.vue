2cf6ce9054ec7a82897e05bc2e3177100c47081c
Nikolai Maidan <axroit.08@gmail.com>
2023-10-09 19:34:29 +0800

    feat: new main component

---
+++ src/popup/components/MainPage/MainPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-page">
+    <input class="main-page__input" type="text" placeholder="Input your txn hash">
+    <div class="main-page__image">
+    </div>
+  </div>
+  </template>
+  
+<script lang="ts" setup></script>
+  
+<style lang="scss" scoped>
+.main-page {
+  margin-top: 30px;
+  &__input {
+    display: block;
+    border-radius: 8px;
+    border: 1px solid #000;
+  }
+}
+</style>
