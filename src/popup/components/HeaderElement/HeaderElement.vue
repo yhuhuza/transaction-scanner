8ae7d66652bbe36b1de386f2b3b2ca0efdc92476
Nikolai Maidan <axroit.08@gmail.com>
2023-10-10 22:39:09 +0800

    ref: delete useless code + refactoring

--- src/popup/components/HeaderElement/HeaderElement.vue
+++ src/popup/components/HeaderElement/HeaderElement.vue
@@ -1,47 +1,15 @@
 <template>
-    <header class="header-element">
-      <div class="header-element__wrapper">
+    <header class="flex flex-row justify-between items-center box-border justify-around">
+      <div>
         <img src="../../../assets/logo/Logotype.svg" />
       </div>
-      <select id="select" class="header-element__select" name="plan">
-        <option value="none" selected>Tether TRC20</option>
-        <option value="free">Bitcoin</option>
-        <option value="starter">Ethereum</option>
-        <option value="professional">Toncoin</option>
-        <option value="corporate">Shiba Inu</option>
-      </select>
-      <router-link to="/settings" class="header-element__settings">
+      <network-changer></network-changer>
+      <router-link to="/settings" class="h-7 w-7 cursor-pointer">
         <img src="../../../assets/logo/settings-sign.svg" />
       </router-link>
     </header>
 </template>
   
-<script lang="ts" setup></script>
-  
-<style lang="scss" scoped>
-
-.header-element {
-  box-sizing: border-box;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  &__select {
-    box-sizing: border-box;
-    position: relative;
-    border: none;
-    padding: 10px;
-    width: 200px;
-    height: 40px;
-    background-color: #03B7E2;
-    border-radius: 16px;
-    color: #FFFFFF;
-    font-size: 16px;
-    font-weight: 600;
-  }
-  &__settings {
-    cursor: pointer;
-    width: 30px;
-    height: 30px;
-  }
-}
-</style>
+<script lang="ts" setup>
+import NetworkChanger from '../NetworkChanger/NetworkChanger.vue';
+</script>
