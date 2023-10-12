1e8096c33ac5ff23183a69654da5b63af3784e13
Nikolai Maidan <axroit.08@gmail.com>
2023-10-12 16:09:50 +0800

    style: main page develop

--- src/popup/components/MainPage/MainPage.vue
+++ src/popup/components/MainPage/MainPage.vue
@@ -1,20 +1,19 @@
-<template>
-  <div class="main-page">
-    <input class="main-page__input" type="text" placeholder="Input your txn hash">
-    <div class="main-page__image">
-    </div>
-  </div>
-  </template>
-  
 <script lang="ts" setup></script>
-  
-<style lang="scss" scoped>
-.main-page {
-  margin-top: 30px;
-  &__input {
-    display: block;
-    border-radius: 8px;
-    border: 1px solid #000;
-  }
-}
-</style>
+
+<template>
+  <div class="mt-8">
+
+    <div class="border border-black rounded-lg w-full flex flex-row py-2.5 px-3.5 h-11 align-center justify-between items-center">
+      <input class="outline-none input-text h-8 w-full" type="text" placeholder="Input your txn hash">
+      <div class="ml-2">
+        <img class="cursor-pointer" src="../../../assets/logo/search-sign.svg" alr="search" />
+      </div>
+    </div>
+
+    <div class="w-full h-80 mt-9 flex items-center">
+      <img class="w-full h-56" src="../../../assets/logo/placeholder-light.png" alt="placeholder" />
+    </div>
+
+    <button class="uppercase button-history mt-4 rounded-lg border-black heading2 w-full h-12 py-2">Transaction history</button>
+  </div>
+</template>
