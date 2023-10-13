c994909e3a19dbb93dd7038c356611e91fe8f8df
Nikolai Maidan <axroit.08@gmail.com>
2023-10-13 12:48:19 +0800

    style: settings menu dark mode implement

--- src/popup/components/SettingsElement/SettingsMenu.vue
+++ src/popup/components/SettingsElement/SettingsMenu.vue
@@ -1,66 +1,18 @@
-<template>
-  <section class="settings-menu">
-    <div v-for="item in SETTINGS_MENU" :key="item.title" class="settings-menu__button">
-      <div class="settings-menu__button_logo">
-        <img :src="item.image" :alt="item.alt" />
-      </div>
-      <p class="settings-menu__button_text">{{ item.title }}</p>
-      <div class="settings-menu__button_arrow">
-        <img src="../../../assets/logo/arrow-forward.svg" alt="arrow" />
-      </div>
-    </div>
-  </section>
-</template>
-    
 <script lang="ts" setup>
 import { SETTINGS_MENU } from '../../../utils/constants';
 
 </script>
-    
-<style lang="scss" scoped>
-.settings-menu {
-box-sizing: border-box;
-height: inherit;
-&__header {
-    display: flex;
-    align-items: center;
-    margin-bottom: 30px;
-    &_text {
-      font-size: 24px;
-      font-family: 'Monserrat Regular', sans-serif;
-    }
-}
-&__arrow-back {
-    margin-right: 25%;
-}
-&__background-color {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: 30px;
-}
-&__logo {
-    margin: 0 20px;
-    width: 28px;
-    height: 28px;
-}
-&__menu {
-    display: flex;
-    flex-direction: column;
-}
-&__button {
-    cursor: pointer;
-    padding: 20px 30px;
-    border-radius: 16px;
-    border: 1px solid #000;
-    margin: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    &_logo {
-    width: 24px;
-    height: 24px;
-    }
-  }
-}
-</style>
+
+<template>
+  <section class="mt-12">
+    <div v-for="item in SETTINGS_MENU" :key="item.title" class="overflow-hidden border dark:border-white rounded-lg flex items-center mb-6 cursor-pointer w-full h-16">
+      <div class="w-2/12 h-full mr-2 flex items-center justify-center">
+        <img class="w-7 h-7 dark:invert" :src="item.image" :alt="item.alt" />
+      </div>
+      <p class="flex-start text mr-auto dark:text-white">{{ item.title }}</p>
+      <div class="w-2/12 flex items-center justify-center h-full">
+        <img class="dark:invert" src="../../../assets/logo/arrow-forward.svg" alt="arrow" />
+      </div>
+    </div>
+  </section>
+</template>
