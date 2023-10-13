8cd1a7c76fe54c181d94044e708df052900e4def
Nikolai Maidan <axroit.08@gmail.com>
2023-10-13 14:29:02 +0800

    fix: change way how to change dark mode

--- src/popup/components/SettingsElement/SettingsElement.vue
+++ src/popup/components/SettingsElement/SettingsElement.vue
@@ -1,177 +1,26 @@
 <script lang="ts" setup>
-import { useDark } from '@vueuse/core';
-import { computed } from 'vue';
-
-import LightArrowBack from '../../../assets/logo/arrow-back-light.svg';
-import DarkArrowBack from '../../../assets/logo/arrow-back.svg';
-import DarkThemeMoon from '../../../assets/logo/dark-theme-moon.svg';
-import DarkThemeSun from '../../../assets/logo/dark-theme-sun.svg';
-import LightThemeMoon from '../../../assets/logo/light-theme-moon.svg';
-import LightThemeSun from '../../../assets/logo/light-theme-sun.svg';
+import ToggleButton from '../ToggleButton/ToggleButton.vue';
 
 import SettingsMenu from './SettingsMenu.vue';
-
-const isDark = useDark({
-  selector: "body",
-  attribute: "class",
-  valueDark: "dark",
-  valueLight: "light",
-});
-
-const changeColorScheme = () => {
-  const isDarkTheme = !isDark.value;
-  localStorage.setItem('isDark', JSON.stringify(isDarkTheme));
-  isDark.value = isDarkTheme;
-};
-
-const definedArrow = computed(() => {
-  return isDark.value ? LightArrowBack : DarkArrowBack;
-});
-
-const definedThemeSign = computed(() => {
-  return isDark.value ? DarkThemeSun : LightThemeSun;
-});
-
-const definedThemeMoon = computed(() => {
-  return isDark.value ? DarkThemeMoon : LightThemeMoon;
-});
 </script>
 
 <template>
   <section class="dark:bg-dark-coal border-box">
-
     <div class="flex items-center mb-7">
-      <router-link to="/index.html" class="w-10 h-10">
-        <img class="h-full" :src="definedArrow" alt="arrow"/>
+      <router-link class="w-10 h-10" to="/index.html" >
+        <img class="h-full dark:invert" src="../../../assets/logo/arrow-back.svg" alt="arrow"/>
       </router-link>
       <h2 class="dark:text-white w-full pl-ef heading1 text-black text-xl uppercase">Settings</h2>
     </div>
-
-    <div class="settings-element__background-color">
-      <div class="settings-element__logo">
-        <img :src="definedThemeSign" alt="moon" />
+    <div class="mb-8 flex items-center justify-center">
+      <div class="w-7 h-7 mr-5">
+        <img class="h-full dark:invert" src="../../../assets/logo/theme-sun.svg" alt="moon" />
       </div>
-
-      <label class="switch">
-        <input type="checkbox" :checked="isDark" @click="changeColorScheme()">
-        <span class="slider round"></span>
-      </label> 
-
-      <div class="settings-element__logo">
-        <img :src=" definedThemeMoon" alt="moon" />
+      <toggle-button></toggle-button>
+      <div class="w-7 h-7 ml-5 ">
+        <img class="h-full dark:invert" src="../../../assets/logo/theme-moon.svg" alt="moon" />
       </div>
     </div>
-
     <settings-menu></settings-menu>
   </section>
 </template>
-  
-<style lang="scss" scoped>
-.settings-element {
-  box-sizing: border-box;
-  height: inherit;
-  &__header {
-    display: flex;
-    align-items: center;
-    margin-bottom: 30px;
-    &_text {
-      font-family: Monserrat Regular, sans-serif;
-      font-size: 24px;
-    }
-  }
-  &__arrow-back {
-    margin-right: 25%;
-  }
-  &__background-color {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: 30px;
-  }
-  &__logo {
-    margin: 0 20px;
-    width: 28px;
-    height: 28px;
-  }
-  &__menu {
-    display: flex;
-    flex-direction: column;
-  }
-  &__button {
-    cursor: pointer;
-    padding: 20px 30px;
-    border-radius: 16px;
-    border: 1px solid #000;
-    margin: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    &_logo {
-      width: 24px;
-      height: 24px;
-    }
-  }
-}
-
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 68px;
-  height: 32px;
-}
-
-/* Hide default HTML checkbox */
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-/* The slider */
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: 3px;
-  background-color: white;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-input:checked + .slider {
-  background-color: #2196F3;
-}
-
-input:focus + .slider {
-  box-shadow: 0 0 1px #2196F3;
-}
-
-input:checked + .slider:before {
-  -webkit-transform: translateX(32px);
-  -ms-transform: translateX(32px);
-  transform: translateX(32px);
-}
-
-/* Rounded sliders */
-.slider.round {
-  border-radius: 34px;
-}
-
-.slider.round:before {
-  border-radius: 50%;
-}
-  </style>
