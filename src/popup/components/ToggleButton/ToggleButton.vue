cccbb0feba39e1c8b854f1c1f63ade488e8f0eab
Nikolai Maidan <axroit.08@gmail.com>
2023-10-13 14:28:23 +0800

    feat: create component for toggle button

---
+++ src/popup/components/ToggleButton/ToggleButton.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { useDark } from '@vueuse/core';
+
+const isDark = useDark({
+  selector: "body",
+  attribute: "class",
+  valueDark: "dark",
+  valueLight: "light",
+});
+
+const changeColorScheme = () => {
+  const isDarkTheme = !isDark.value;
+  localStorage.setItem('isDark', JSON.stringify(isDarkTheme));
+  isDark.value = isDarkTheme;
+};
+</script>
+
+<template>
+    <label class="switch">
+    <input type="checkbox" :checked="isDark" @click="changeColorScheme()">
+    <span class="slider round"></span>
+    </label> 
+</template>
