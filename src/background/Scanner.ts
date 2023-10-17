824b57d2f4928c3989f8ddb484733c5adb0c31d3
Nikolai Maidan <axroit.08@gmail.com>
2023-10-17 21:50:44 +0800

    feat: logic of making requests

--- src/background/Scanner.ts
+++ src/background/Scanner.ts
@@ -1,5 +1,9 @@
 import browser from 'webextension-polyfill';
 
+// import { AllowedType } from '../types/methodsKEys';
+
+import { API_KEY } from './constants/constants';
+
 export default class Scanner {
 
   constructor() {
@@ -8,5 +12,18 @@ export default class Scanner {
 
   showInstallation(): void {
     console.log('I am installed');
+  }
+
+  async getInformationAboutTransaction(data: any): Promise<void> {
+    const { request } = data;
+    const url: string = `https://apilist.tronscanapi.com/api/transaction-info?hash=${request.data.hash}`;
+    fetch(url, {
+      headers: {
+        'TRON-PRO-API-KEY': API_KEY
+      }
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
   }
 }
