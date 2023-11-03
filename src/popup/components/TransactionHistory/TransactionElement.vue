9dffed11d1c22b3569a7b8303c077ba895ec41b0
Nikolai Maidan <axroit.08@gmail.com>
2023-11-03 16:55:48 +0100

    feat: add two components for history

---
+++ src/popup/components/TransactionHistory/TransactionElement.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+import { ParsedTransaction } from '../../../types/scanner';
+
+
+const { t } = useI18n();
+const props = defineProps<{ transaction?: ParsedTransaction }>();
+const transactionDetails = ref(props.transaction);
+</script>
+
+<template>
+  <article class="mb-4 border border-neutral-300 p-3 rounded">
+    <div class="small-bold-text dark:text-white">{{ transactionDetails?.timeRange }}</div>
+    <div class="flex items-center justify-between mt-2 dark-semibold-text">
+      <span class="dark:text-white">{{ transactionDetails?.transferredStatus }}</span>
+      <span class="dark:text-white">{{ transactionDetails?.transferredValue }}</span>
+      <span class="dark:text-white">{{ transactionDetails?.transactionSymbol }}</span>
+      <span class="dark:text-white">{{ transactionDetails?.resultValue }}</span>
+      <span class="dark:text-white">{{ transactionDetails?.confirmedStatus }}</span>
+    </div>
+    <div class="mt-4 flex items-center justify-between input-text">
+      <button class="px-6 py-2 border rounded-lg dark:text-white">{{ t('history.details') }}</button>
+      <div
+          class="
+          download-pdf px-5 py-2 border rounded-lg border-marine dark:text-white
+          bg-marine white-bold-text cursor-pointer flex items-center justify-between">
+          PDF
+      </div>
+    </div>
+  </article>
+</template>
--- src/popup/components/TransactionHistory/TransactionHistory.vue
+++ src/popup/components/TransactionHistory/TransactionHistory.vue
@@ -1,23 +1,28 @@
 <script lang="ts" setup>
 import { storeToRefs } from 'pinia';
 import { useI18n } from 'vue-i18n';
+import { useRoute } from 'vue-router';
 
 import { useTransactionsStore } from '../../stores/useTransactionsStore';
 
+import TransactionElement from './TransactionElement.vue';
+
 const { t } = useI18n();
+const router = useRoute();
+console.log('Router', router);
 const transactionStore = useTransactionsStore();
 
 const { savedTransactions } = storeToRefs(transactionStore);
 </script>
 
 <template>
-    <div class="flex items-center mb-7">
+    <div class="flex items-center">
       <router-link class="w-10 h-10" to="/index.html" >
         <img class="h-full dark:invert" src="../../../assets/logo/arrow-back.svg" alt="arrow"/>
       </router-link>
       <h2 class="dark:text-white w-full pl-7 heading1 text-black text-xl uppercase">{{ t('history.title') }}</h2>
     </div>
-    <div>
+    <div class="pt-6 pb-2 scroll-auto">
         <TransactionElement
             v-for="item in savedTransactions"
             :key="item.hashValue" 
